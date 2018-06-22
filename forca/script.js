4b1690aa78a5fea1cb34272c7359d03e6c32eeaf
Tiemi Salli <tsalli8@yahoo.com.br>
2018-06-22 17:57:14 -0300

    Comando para escolher palavras aleatorias

--- forca/script.js
+++ forca/script.js
@@ -1,6 +1,28 @@
+function obterPalavra (){
+    let texto = new Array ();
+    texto[0] = ["M","A","S","T","E","R","T","E","C","H"];
+    texto[1] = ["F","A","C","E","B","O","O","K","",""];
+    texto[2] = ["P","R","O","G","R","A","M","A","R",""];
+    texto[3] = ["E","S","T","U","D","A","R","","",""];
+    texto[4] = ["T","R","A","B","A","L","H","O","",""];
+    texto[5] = ["J","A","V","A","S","C","R","I","P","T"];
+    texto[6] = ["C","O","M","P","U","T","A","D","O","R"];
+    texto[7] = ["E","S","T","A","C","A","O","","",""];
 
-let resposta = prompt ("Digite uma letra");
+    let i = Math.floor(Math.random()*8);
+ console.log(texto[i]);
+ 
+    document.querySelector("#casaUm").innerHTML = (texto[i][0]);
+    document.querySelector("#casaDois").innerHTML = (texto[i][1]);
+    document.querySelector("#casaTres").innerHTML = (texto[i][2]);
+    document.querySelector("#casaQuatro").innerHTML = (texto[i][3]);
+    document.querySelector("#casaCinco").innerHTML = (texto[i][4]);
+    document.querySelector("#casaSeis").innerHTML = (texto[i][5]);
+    document.querySelector("#casaSete").innerHTML = (texto[i][6]);
+    document.querySelector("#casaOito").innerHTML = (texto[i][7]);
+    document.querySelector("#casaNove").innerHTML = (texto[i][8]);
+    document.querySelector("#casaDez").innerHTML = (texto[i][9]);
 
-function comparandos () {
-   
 }
+
+onClick=obterPalavra();
--- velha/script.js
+++ velha/script.js
@@ -1,46 +1,77 @@
-let vez = 1;
-let vencedor = "";
+let casa1 = document.querySelector("#casa1");
+let casa2 = document.querySelector("#casa2");
+let casa3 = document.querySelector("#casa3");
+let casa4 = document.querySelector("#casa4");
+let casa5 = document.querySelector("#casa5");
+let casa6 = document.querySelector("#casa6");
+let casa7 = document.querySelector("#casa7");
+let casa8 = document.querySelector("#casa8");
+let casa9 = document.querySelector("#casa9");
+let quadradinho = [casa1,casa2,casa3,casa4,casa5,casa6,casa7,casa8,casa9]
+let jogadorUm = 'X';
+let ganhador = false;
+let jogadorDois = 'O';
+let i = 1;
 
-velha (function(){
+function diagonal(){
+    if((casa1.innerHTML === "X" && casa2.innerHTML === "X" &&  casa2.innerHTML === "X") || (casa4.innerHTML === "X" && casa5.innerHTML === "X" && casa6.innerHTML === "X") || (casa7.innerHTML === "X" && casa8.innerHTML === "X" && casa9.innerHTML === "X")){
+        encerrar();
+        ganhador = true;
+    }
+    
+    if((casa1.innerHTML === "X" && casa4.innerHTML === "X" &&  casa7.innerHTML === "X") || (casa2.innerHTML === "X" && casa5.innerHTML === "X" && casa8.innerHTML === "X") || (casa3.innerHTML === "X" && casa6.innerHTML === "X" && casa9.innerHTML === "X")){
+        encerrar();
+        ganhador = true;
+    }
+}
 
-});
+function encerrar(){
+    
+    for(let quadradinho of quadrados){
+        quadradinho.onclick = null;
+    }
 
-    function casasIguais(a, b, c){
-        let casaA = velha("#casa"+a);
-        let casaB = velha("#casa"+b);
-        let casaC = velha("#casa"+c);
-        let bgA = velha("#casa"+a).css("background-image");
-        let bgB = velha("#casa"+b).css("background-image");
-        let bgC = velha("#casa"+c).css("background-image");
-        if( (bgA === bgB) && (bgB === bgC) && (bgA !== "none" && bgA !== "")){
-            if(bgA.indexOf("x.png") >= 0)
-                vencedor = "1";
-            else
-                vencedor = "2";
-            return true;
+    h1.innerHTML = "Parábens,  Você Ganhou!";
+}
+
+function jogar(){
+
+    if(!ganhador && i === 9){
+        h1.innerHTML = 'VELHA !!';
+    }
+
+    if(i % 2 === 0){
+        this.innerHTML = jogadorDois;
+    }
+
+    else{
+        this.innerHTML = jogadorUm;
+    }
+
+    i++;
+    this.onclick = null;
+    verificarFim();
+}
+
+
+function verificarFim(){
+    for(let j = 0; j < 9; j += 3){   
+        if(quadrados[j].innerHTML !== '.' && quadrados[j].innerHTML === quadrados[j + 1].innerHTML && quadrados[j + 1].innerHTML === quadrados[j + 2].innerHTML){
+            encerrar();
+            ganhou = true;
         }
-        else{
-            return false;
+    }
+    for(let j = 0; j < 3; j += 1){   
+        if(quadrados[j].innerHTML !== '.' && quadrados[j].innerHTML === quadrados[j + 3].innerHTML && quadrados[j + 3].innerHTML === quadrados[j + 6].innerHTML){
+            encerrar();
+            ganhou = true;
         }
     }
     
-    function verificarFimDeJogo(){
-        if( casasIguais(1, 2, 3) || casasIguais(4, 5, 6) || casasIguais(7, 8, 9) ||
-            casasIguais(1, 4, 7) || casasIguais(2, 5, 8) || casasIguais(3, 6, 9) ||
-            casasIguais(1, 5, 9) || casasIguais(3, 5, 7)
-            ){
-                velha("#resultado").html("<h1>O jogador " + vencedor + "venceu! </h1>");
-                velha(".casa").off("click");
-        }
-    }
-    
-    velha(".casa").click(function(){
-        var bg = velha(this).css("background-image");
-        if(bg === "none" || bg === "")
-        {          
-            var fig = "url(" + vez.toString() + ".png)";
-            velha(this).css("background", fig);
-            vez = (vez === 1? 2:1); 
-            verificarFimDeJogo();
-        }
-    });
+    diagonal();    
+}
+
+for(let quadrado of quadrados){
+    quadrados.onclick = jogada;
+}
+
